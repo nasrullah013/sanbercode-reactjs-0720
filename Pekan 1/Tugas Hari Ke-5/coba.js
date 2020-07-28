55d1d17f1c5fb237838b56cc3f0c4e99e8bf1f53
nasrullah013 <dani.nasrullah1998@gmail.com>
2020-07-28 07:00:21 +0700

    Update Tugas Hari Ke-1 Pekan 2

---
+++ Pekan 1/Tugas Hari Ke-5/coba.js
@@ -0,0 +1,23 @@
+// function
+function luasLingkaran(jari) {
+    const phi = 3.14
+
+    // var r = parseFloat(jari)
+    
+    return ll = phi * jari * jarif
+}
+
+
+
+// deklarasi variabel luas lingkaran
+var jari_jari = 7
+console.log(luasLingkaran(jari_jari))
+
+// deklarasi variabel luas segitiga
+var alas
+var tinggi
+
+// deklarasi variabel luas persegi
+var panjang
+var lebar
+
