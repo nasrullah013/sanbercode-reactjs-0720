1271f895eb4cfeb86e83a391372ca81d05d0af7a
nasrullah013 <dani.nasrullah1998@gmail.com>
2020-07-31 21:53:08 +0700

    Update Tugas Hari Ke-4 Pekan 2

---
+++ Pekan 2/Tugas Hari Ke-4/Tugas9.js
@@ -0,0 +1,54 @@
+// Soal 1
+
+console.log("\n----SOAL 1----")
+
+const newFunction = function literal(firstName, lastName) {
+    return {
+        firstName,
+        lastName,
+        fullName() {
+            console.log(firstName + " " + lastName)
+            return
+        }
+    }
+}
+
+
+//Driver Code 
+newFunction("William", "Imoh").fullName()
+
+// Soal 2 
+
+console.log("\n----SOAL 2----")
+
+const newObject = {
+    firstName: "Harry",
+    lastName: "Potter Holt",
+    destination: "Hogwarts React Conf",
+    occupation: "Deve-wizard Avocado",
+    spell: "Vimulus Renderus!!!"
+}
+
+const {
+    firstName,
+    lastName,
+    destination,
+    occupation,
+    spell
+} = newObject
+
+console.log(newObject)
+
+console.log(firstName, lastName, destination, occupation)
+
+// soal 3
+
+console.log("\n----SOAL 3----")
+
+const west = ["Will", "Chris", "Sam", "Holly"]
+const east = ["Gill", "Brian", "Noel", "Maggie"]
+
+
+const combined = [...west, ...east]
+
+console.log(combined)
